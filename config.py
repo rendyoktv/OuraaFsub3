b5b2ab6f0332d35dec470b2d2e8c9d5c4b52b817
rendyoktv <rendyinsnae@gmail.com>
2022-09-11 16:21:49 +0800

    resolve

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ OWNER = os.environ.get("OWNER", "lahlausokap")
 PROTECT_CONTENT = os.environ.get("PROTECT_CONTENT", "False")
 
 # Database
-DB_URI = os.environ.get("DATABASE_URL", "postgres://docker:password@localhost/docker")
+DB_URI = os.environ.get("DATABASE_URL", "postgres://bots:password@localhost/bots")
 
 # Username CH & Group
 CHANNEL = os.environ.get("CHANNEL", "darksidefcx")
